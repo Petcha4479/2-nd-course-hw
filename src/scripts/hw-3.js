f1dfa633bfe85d9d401bf4d1419bdb8f8b00b987
Petcha4479 <petcha4479@gmail.com>
2025-04-19 19:16:29 +0400

    5 homework

--- src/scripts/hw-3.js
+++ src/scripts/hw-3.js
@@ -1,79 +1,79 @@
-// let password = 'пароль';
-// let request = prompt(String('Введите пароль'));
-// if (request === password) {
-//     alert('Пароль введен верно');
-// } else {
-//     alert('Пароль введен неправильно');
-// }
-//
-// let c = 2;
-// if (c > 0 && c <= 10) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
-// }
-//
-// let d = 22;
-// let e = 33;
-// if (d > 100 || e > 100) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
-// }
-//
-// let a = '2';
-// let b = '3';
-// a = Number(a);
-// b = Number(b);
-// alert(a + b);
-//
-// let monthNumber = 3;
-// if (monthNumber > 12) {
-//     console.log('номер месяца больше 12')
-// } else {
-//     switch (monthNumber) {
-//         case 1:
-//             console.log('зима');
-//             break;
-//         case 2:
-//             console.log('зима');
-//             break;
-//         case 3:
-//             console.log('весна');
-//             break;
-//         case 4:
-//             console.log('весна');
-//             break;
-//         case 5:
-//             console.log('весна');
-//             break;
-//         case 6:
-//             console.log('лето');
-//             break;
-//         case 7:
-//             console.log('лето');
-//             break;
-//         case 8:
-//             console.log('лето');
-//             break;
-//         case 9:
-//             console.log('осень');
-//             break;
-//         case 10:
-//             console.log('осень');
-//             break;
-//         case 11:
-//             console.log('осень');
-//             break;
-//         case 12:
-//             console.log('зима');
-//             break;
-//         default:
-//             console.log('что ты такое');
-//     }
-// }
-//
-//
-//
-//
-//
+let password = 'пароль';
+let request = prompt(String('Введите пароль'));
+if (request === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
+
+let c = 2;
+if (c > 0 && c <= 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+let d = 22;
+let e = 33;
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+let a = '2';
+let b = '3';
+a = Number(a);
+b = Number(b);
+alert(a + b);
+
+let monthNumber = 3;
+if (monthNumber > 12) {
+    console.log('номер месяца больше 12')
+} else {
+    switch (monthNumber) {
+        case 1:
+            console.log('зима');
+            break;
+        case 2:
+            console.log('зима');
+            break;
+        case 3:
+            console.log('весна');
+            break;
+        case 4:
+            console.log('весна');
+            break;
+        case 5:
+            console.log('весна');
+            break;
+        case 6:
+            console.log('лето');
+            break;
+        case 7:
+            console.log('лето');
+            break;
+        case 8:
+            console.log('лето');
+            break;
+        case 9:
+            console.log('осень');
+            break;
+        case 10:
+            console.log('осень');
+            break;
+        case 11:
+            console.log('осень');
+            break;
+        case 12:
+            console.log('зима');
+            break;
+        default:
+            console.log('что ты такое');
+    }
+}
+
+
+
+
+
---
+++ src/scripts/hw-5.js
@@ -0,0 +1,18 @@
+// Игра угадай число
+function firstGame () {
+    alert('клик работает')
+}
+
+
+
+// Задание 1
+function min (a, b) {
+    return a < b ? a : b;
+}
+console.log(min(8, 7));
+
+// Задание 2
+function checkNumber (num) {
+    return (num % 2 === 0) ? 'Число четное' : 'Число нечетное';
+}
+console.log(checkNumber(10));
